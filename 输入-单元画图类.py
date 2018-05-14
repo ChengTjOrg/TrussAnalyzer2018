f3f89cf84e9d48331b079478363e648e620b80ee
gjmarmalade <37096746+gjmarmalade@users.noreply.github.com>
2018-05-15 00:54:00 +0800

    Update 输入-单元画图类.py

--- 输入-单元画图类.py
+++ 输入-单元画图类.py
@@ -10,36 +10,33 @@ Created on Mon May 14 17:48:43 2018
 #import numpy as np
 from __future__ import division
 import matplotlib.pyplot as plt
+import pylab as plb
 
 
 class PreUnitDisplay:
-    def __init__(self,a1,b1,a2,b2,s1,s2,lx,ly,tc,pc,sc):#依次为x坐标/y坐标/支座情况/画布大小/构件颜色,lx,ly只需全局输入一次
+    def __init__(self,a1,b1,a2,b2,s1,s2,l,tc,pc,sc):#依次为x坐标/y坐标/支座情况/画布大小/构件颜色,lx,ly只需全局输入一次
         self.a1=a1
         self.b1=b1
         self.a2=a2
         self.b2=b2
         self.s1=s1
         self.s2=s2
-        self.lx=lx
-        self.ly=ly
-        self.l=min(self.lx,self.ly)
-        self.c1=1.2#杆端圆圈大小系数
-        self.c2=1/18#杆端圆圈线宽系数
-        self.c3=1/10#杆线宽系数
+        self.l=l
         self.tc=tc#杆颜色
         self.pc=pc#端点颜色
         self.sc=sc#支座颜色
     def display(self):
-        plt.xlim(0,self.lx)
-        plt.ylim(0,self.ly)
+        plt.xlim(0,self.l)
+        plt.ylim(0,self.l)
+        plt.figure(figsize=(self.l,self.l))
         #画两端结点
         x=[self.a1,self.a2]
         y=[self.b1,self.b2]
         '''plt.plot(x[0], y[0], 'C3', zorder=1,lw=3)#zorder用来标记显示顺序（层次）
         plt.plot(x[1], y[1], 'C3', zorder=1,lw=3)'''
-        plt.scatter(x,y,c='w',marker='o', s=self.c1*self.l,edgecolors=self.pc,linewidths=self.c2*self.l, zorder=100)#s是点大小
+        plt.scatter(x,y,c='w',marker='o', s=60,edgecolors=self.pc,linewidths=3, zorder=100)#s是点大小
         #画杆
-        plt.plot(x,y,c=self.tc,linewidth=self.c3*self.l,zorder=1)
+        plt.plot(x,y,c=self.tc,linewidth=5,zorder=1)
         #画支座
         sp1=Support(self.s1,self.a1,self.b1,self.l,self.sc)
         sp1.display()
@@ -56,9 +53,6 @@ class Support:
         self.l=l
         self.c1=1/15#支座长短系数1
         self.c2=1/30#支座长短系数2（大地）
-        self.c3=1#支座圆圈大小s系数
-        self.c4=1/25#支座圆圈线宽系数
-        self.c5=1/20#支座线条宽度系数
         self.sc=sc
     def none(self):
         pass
@@ -66,20 +60,20 @@ class Support:
         #仅有水平支座，向左画
         x=[self.a,self.a-self.c1*self.l]
         y=[self.b,self.b]
-        plt.scatter(x[1],y[1],c='w',marker='o',s=self.c3*self.l,edgecolors=self.sc,linewidths=self.c4*self.l,zorder=100)
-        plt.plot(x,y,c=self.sc,linewidth=self.c5*self.l,zorder=1)
+        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
+        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
         x1=[self.a-self.c1*self.l,self.a-self.c1*self.l]
         y1=[self.b+self.c2*self.l,self.b-self.c2*self.l]
-        plt.plot(x1,y1,c=self.sc,linewidth=self.c5*self.l,zorder=1)
+        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
     def vertical(self):
         #仅有竖直支座，向下画
         x=[self.a,self.a]
         y=[self.b,self.b-self.c1*self.l]
-        plt.scatter(x[1],y[1],c='w',marker='o',s=self.c3*self.l,edgecolors=self.sc,linewidths=self.c4*self.l,zorder=100)
-        plt.plot(x,y,c=self.sc,linewidth=self.c5*self.l,zorder=1)
+        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
+        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
         x1=[self.a-self.c2*self.l,self.a+self.c2*self.l]
         y1=[self.b-self.c1*self.l,self.b-self.c1*self.l]
-        plt.plot(x1,y1,c=self.sc,linewidth=self.c5*self.l,zorder=1)
+        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
     def both(self):
         #有两个方向的支座
         Support.horizontal(self)
@@ -90,7 +84,7 @@ class Support:
     '''要注意支座图像的放置方向和大小不要与杆件重叠!!'''
    
 
-pud=PreUnitDisplay(25,10,40,22,3,1,100,50,'red','blue','black')
+pud=PreUnitDisplay(2.5,1.0,4.0,2.2,3,1,5.0,'black','black','black')
 pud.display()     
        
 
