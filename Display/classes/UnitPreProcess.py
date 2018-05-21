e7a1ef7ea4b2260bbce1d1ee9de8dbac79226908
gjmarmalade <37096746+gjmarmalade@users.noreply.github.com>
2018-05-21 23:33:43 +0800

    Update UnitPreProcess.py

--- Display/classes/UnitPreProcess.py
+++ Display/classes/UnitPreProcess.py
@@ -11,16 +11,18 @@ Created on Mon May 14 17:48:43 2018
 from __future__ import division
 import matplotlib.pyplot as plt
 #import pylab as plb
-
+import Support
 
 class UnitPreProcess:
-    def __init__(self,a1,b1,a2,b2,s1,s2,l,tc,pc,sc):#依次为x坐标/y坐标/支座情况/画布大小/构件颜色,lx,ly只需全局输入一次
+    def __init__(self,a1,b1,a2,b2,sx1,sy1,sx2,sy2,l,tc,pc,sc):#依次为x坐标/y坐标/支座情况/画布大小/构件颜色,lx,ly只需全局输入一次
         self.a1=a1
         self.b1=b1
         self.a2=a2
         self.b2=b2
-        self.s1=s1
-        self.s2=s2
+        self.sx1=sx1
+        self.sy1=sy1
+        self.sx2=sx2
+        self.sy2=sy2
         self.l=l
         self.tc=tc#杆颜色
         self.pc=pc#端点颜色
@@ -39,9 +41,9 @@ class UnitPreProcess:
         #画杆
         plt.plot(x,y,c=self.tc,linewidth=5,zorder=1)
         #画支座
-        sp1=Support(self.s1,self.a1,self.b1,self.l,self.sc)
+        sp1=Support.Support(self.sx1,self.sy1,self.a1,self.b1,self.l,self.sc)
         sp1.display()
-        sp2=Support(self.s2,self.a2,self.b2,self.l,self.sc)
+        sp2=Support.Support(self.sx2,self.sy2,self.a2,self.b2,self.l,self.sc)
         sp2.display()
         plt.tight_layout()
     def savefig(self):
@@ -49,46 +51,7 @@ class UnitPreProcess:
         plt.show()
 
 
-class Support:
-    def __init__(self,s,a,b,l,sc):
-        self.s=s
-        self.a=a
-        self.b=b
-        self.l=l
-        self.c1=1/25#支座长短系数1
-        self.c2=1/50#支座长短系数2（大地）
-        self.sc=sc
-    def none(self):
-        pass
-    def horizontal(self):
-        #仅有水平支座，向左画
-        x=[self.a,self.a-self.c1*self.l]
-        y=[self.b,self.b]
-        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
-        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
-        x1=[self.a-self.c1*self.l,self.a-self.c1*self.l]
-        y1=[self.b+self.c2*self.l,self.b-self.c2*self.l]
-        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
-    def vertical(self):
-        #仅有竖直支座，向下画
-        x=[self.a,self.a]
-        y=[self.b,self.b-self.c1*self.l]
-        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
-        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
-        x1=[self.a-self.c2*self.l,self.a+self.c2*self.l]
-        y1=[self.b-self.c1*self.l,self.b-self.c1*self.l]
-        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
-    def both(self):
-        #有两个方向的支座
-        Support.horizontal(self)
-        Support.vertical(self)
-    def display(self):
-        self.switcher={0:Support.none,1:Support.horizontal,2:Support.vertical,3:Support.both}
-        self.switcher.get(self.s)(self)
-    '''要注意支座图像的放置方向和大小不要与杆件重叠!!'''
-   
-
-upp=UnitPreProcess(2.5,1.0,4.0,2.2,3,1,5.0,'k','k','b')
+upp=UnitPreProcess(2.5,1.0,4.0,2.2,1,1,1,0,5.0,'k','k','b')
 upp.setfig()
 upp.plot()
 upp.savefig()     
