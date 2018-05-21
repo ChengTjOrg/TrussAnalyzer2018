aa2eec7fa6a90123ad7ec482bedc466dd6898560
gjmarmalade <37096746+gjmarmalade@users.noreply.github.com>
2018-05-21 23:24:20 +0800

    Update UnitPreProcess.py

--- Display/classes/UnitPreProcess.py
+++ Display/classes/UnitPreProcess.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 15 21:58:04 2018
+Created on Mon May 14 17:48:43 2018
 
 @author: Nicole
 """
 
+#pre display in unit
+
+#import numpy as np
 from __future__ import division
 import matplotlib.pyplot as plt
-import Support
+#import pylab as plb
 
-class PreUnitDisplay:
+
+class UnitPreProcess:
     def __init__(self,a1,b1,a2,b2,s1,s2,l,tc,pc,sc):#依次为x坐标/y坐标/支座情况/画布大小/构件颜色,lx,ly只需全局输入一次
         self.a1=a1
         self.b1=b1
@@ -21,10 +25,11 @@ class PreUnitDisplay:
         self.tc=tc#杆颜色
         self.pc=pc#端点颜色
         self.sc=sc#支座颜色
-    def display(self):
+    def setfig(self):
+        plt.figure(figsize=(self.l,self.l))
         plt.xlim(0,self.l)
         plt.ylim(0,self.l)
-        plt.figure(figsize=(self.l,self.l))
+    def plot(self):
         #画两端结点
         x=[self.a1,self.a2]
         y=[self.b1,self.b2]
@@ -39,3 +44,72 @@ class PreUnitDisplay:
         sp2=Support(self.s2,self.a2,self.b2,self.l,self.sc)
         sp2.display()
         plt.tight_layout()
+    def savefig(self):
+        plt.savefig('C:\\文件\\大二下\\绗架结构分析助手\\图像\\前处理\\fig.png',dpi=600)
+        plt.show()
+
+
+class Support:
+    def __init__(self,s,a,b,l,sc):
+        self.s=s
+        self.a=a
+        self.b=b
+        self.l=l
+        self.c1=1/25#支座长短系数1
+        self.c2=1/50#支座长短系数2（大地）
+        self.sc=sc
+    def none(self):
+        pass
+    def horizontal(self):
+        #仅有水平支座，向左画
+        x=[self.a,self.a-self.c1*self.l]
+        y=[self.b,self.b]
+        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
+        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
+        x1=[self.a-self.c1*self.l,self.a-self.c1*self.l]
+        y1=[self.b+self.c2*self.l,self.b-self.c2*self.l]
+        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
+    def vertical(self):
+        #仅有竖直支座，向下画
+        x=[self.a,self.a]
+        y=[self.b,self.b-self.c1*self.l]
+        plt.scatter(x[1],y[1],c='w',marker='o',s=50,edgecolors=self.sc,linewidths=2,zorder=100)
+        plt.plot(x,y,c=self.sc,linewidth=2.5,zorder=1)
+        x1=[self.a-self.c2*self.l,self.a+self.c2*self.l]
+        y1=[self.b-self.c1*self.l,self.b-self.c1*self.l]
+        plt.plot(x1,y1,c=self.sc,linewidth=2.5,zorder=1)
+    def both(self):
+        #有两个方向的支座
+        Support.horizontal(self)
+        Support.vertical(self)
+    def display(self):
+        self.switcher={0:Support.none,1:Support.horizontal,2:Support.vertical,3:Support.both}
+        self.switcher.get(self.s)(self)
+    '''要注意支座图像的放置方向和大小不要与杆件重叠!!'''
+   
+
+upp=UnitPreProcess(2.5,1.0,4.0,2.2,3,1,5.0,'k','k','b')
+upp.setfig()
+upp.plot()
+upp.savefig()     
+       
+
+'''
+问题：
+1.程序中有大段重复的相似语句，不知道有没有办法优化
+2.注释还没有认真写好
+3.支座的方向是单一的，可能会出现重叠遮挡的问题，目前暂时采用了不同颜色作为解决方案，但是这个方案还不够好
+'''
+
+
+    
+'''
+弃用代码：
+def supports(argument):
+    switcher={
+        0:pass
+        1:pass#仅有水平支座
+        2:pass#仅有竖直支座
+        3:pass#有两个方向的支座
+        #要注意支座图像的放置方向和大小不要与杆件重叠!!
+            }'''
