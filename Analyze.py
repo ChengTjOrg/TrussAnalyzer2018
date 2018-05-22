2fc95909bc7aaae2d67be0b6cc7adae22430a48e
2Fzzzzz <834324640@qq.com>
2018-05-22 11:29:31 +0800

    Update Analyze.py

--- Analyze.py
+++ Analyze.py
@@ -7,8 +7,17 @@ Created on Tue May 22 01:46:50 2018
 
 import PostProcess
 
-data=PostProcess.postProcess(6)
-data.loadtruss()
-data.setfig()
-data.plot()
-data.savefig()
+def fig():
+    data=PostProcess.postProcess(6)
+    data.loadtruss()
+    data.setfig()
+    data.plot()
+    data.savefig()
+    
+def fig2():
+    data=PostProcess.postProcess(6)
+    data.loadtruss2()
+    data.setfig()
+    data.plot2()
+    data.savefig()
+    
