4701691a5cc3a7fa196398fe966db3aa7fb1ac9a
gjmarmalade <37096746+gjmarmalade@users.noreply.github.com>
2018-05-22 21:56:00 +0800

    Update Analyze.py

--- Analyze.py
+++ Analyze.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 22 16:06:49 2018
-
-@author: Nicole
-"""
-
-
 import PostProcess
 
 def fig():
@@ -14,13 +6,5 @@ def fig():
     data.setfig()
     data.plot()
     data.savefig()
-'''    
-def fig2():
-    data=PostProcess.postProcess(6)
-    data.loadtruss2()
-    data.setfig()
-    data.plot2()
-    data.savefig()
-'''
-    
+
 fig()
